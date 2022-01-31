a727971f1b477f762e4afad9a3cd23855513a87a
peritissimus <iamkushal.iitkgp@gmail.com>
2022-02-01 01:20:50 +0530

    added export module

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
 
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   assetPrefix: isProd ? '/peritissimus.github.io/' : '',
-  nextConfig
+  reactStrictMode: true,
 }
