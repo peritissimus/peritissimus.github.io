c147386d4a26f5f00f8e77a1c89e017e85bc9599
peritissimus <iamkushal.iitkgp@gmail.com>
2022-02-01 00:50:30 +0530

    ...

--- next.config.js
+++ next.config.js
@@ -3,4 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
 };
 
-module.exports = nextConfig;
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  assetPrefix: isProd ? '/peritissimus.github.io/' : '',
+  nextConfig
+}
