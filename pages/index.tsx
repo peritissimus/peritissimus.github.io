18d56c448384b40303b64c5460b1339a0caa8403
peritissimus <iamkushal.iitkgp@gmail.com>
2022-02-01 01:01:49 +0530

    ...

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,12 +12,10 @@ import {
   useColorModeValue,
 } from "@chakra-ui/react";
 import Main from "../components/layouts/Main";
-import Beta from "../components/v0";
 
 const Home: NextPage = () => (
   <Main title="Dubverse | Prototype V0">
     <Box p={8}>
-      <Beta />
     </Box>
   </Main>
 );
